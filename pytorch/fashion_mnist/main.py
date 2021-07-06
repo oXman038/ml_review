f1cec1cb183f2ab37f2a83ee79604ec248fa9b15
Osman Aka <osmanaka@Osmans-MacBook-Air.local>
2021-07-06 13:15:28 -0700

    fashion_mnist changed to the vanilla form

--- pytorch/fashion_mnist/main.py
+++ pytorch/fashion_mnist/main.py
@@ -43,28 +43,16 @@ def create_model():
             super(NeuralNetwork, self).__init__()
             self.flatten = nn.Flatten()
             self.linear_relu_stack = nn.Sequential(
-                nn.Conv2d(1, 20, 3),
-                nn.ReLU(),
-                nn.Flatten(),
-                nn.Linear(20*26*26, 512),
+                nn.Linear(28*28, 512),
                 nn.ReLU(),
                 nn.Linear(512, 512),
                 nn.ReLU(),
                 nn.Linear(512, 10),
-                nn.ReLU(),
-
-
-
-                # nn.Linear(28*28, 512),
-                # nn.ReLU(),
-                # nn.Linear(512, 512),
-                # nn.ReLU(),
-                # nn.Linear(512, 10),
-                # nn.ReLU()
+                nn.ReLU()
             )
 
         def forward(self, x):
-            # x = self.flatten(x)
+            x = self.flatten(x)
             logits = self.linear_relu_stack(x)
             return logits
 
