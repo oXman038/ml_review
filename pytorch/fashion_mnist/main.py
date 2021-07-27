26def6a406a199d53858256462e1e312c2ebeea4
Osman Aka <osmanaka@Osmans-MacBook-Air.local>
2021-07-26 22:30:40 -0700

    Fixing tabs.

--- pytorch/fashion_mnist/main.py
+++ pytorch/fashion_mnist/main.py
@@ -6,107 +6,107 @@ from torchvision.transforms import ToTensor, Lambda, Compose
 import matplotlib.pyplot as plt
 
 def load_data():
-    # Download training data from open datasets.
-    training_data = datasets.FashionMNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=ToTensor(),
-    )
+  # Download training data from open datasets.
+  training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+  )
 
-    # Download test data from open datasets.
-    test_data = datasets.FashionMNIST(
-        root="data",
-        train=False,
-        download=True,
-        transform=ToTensor(),
-    )
+  # Download test data from open datasets.
+  test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+  )
 
-    batch_size = 64
+  batch_size = 64
 
-    # Create data loaders.
-    train_dataloader = DataLoader(training_data, batch_size=batch_size)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+  # Create data loaders.
+  train_dataloader = DataLoader(training_data, batch_size=batch_size)
+  test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-    for X, y in test_dataloader:
-        print("Shape of X [N, C, H, W]: ", X.shape)
-        print("Shape of y: ", y.shape, y.dtype)
-        break
+  for X, y in test_dataloader:
+    print("Shape of X [N, C, H, W]: ", X.shape)
+    print("Shape of y: ", y.shape, y.dtype)
+    break
 
-    return train_dataloader, test_dataloader
+  return train_dataloader, test_dataloader
 
 
 def create_model():
-    # Define model
-    class NeuralNetwork(nn.Module):
-        def __init__(self):
-            super(NeuralNetwork, self).__init__()
-            self.flatten = nn.Flatten()
-            self.linear_relu_stack = nn.Sequential(
-                nn.Linear(28*28, 512),
-                nn.ReLU(),
-                nn.Linear(512, 512),
-                nn.ReLU(),
-                nn.Linear(512, 10),
-                nn.ReLU()
-            )
+  # Define model
+  class NeuralNetwork(nn.Module):
+    def __init__(self):
+      super(NeuralNetwork, self).__init__()
+      self.flatten = nn.Flatten()
+      self.linear_relu_stack = nn.Sequential(
+        nn.Linear(28*28, 512),
+        nn.ReLU(),
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        nn.Linear(512, 10),
+        nn.ReLU()
+      )
 
-        def forward(self, x):
-            x = self.flatten(x)
-            logits = self.linear_relu_stack(x)
-            return logits
+    def forward(self, x):
+      x = self.flatten(x)
+      logits = self.linear_relu_stack(x)
+      return logits
 
-    model = NeuralNetwork().to(device)
-    print(model)
+  model = NeuralNetwork().to(device)
+  print(model)
 
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-    return model, loss_fn, optimizer
+  loss_fn = nn.CrossEntropyLoss()
+  optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+  return model, loss_fn, optimizer
 
 
 def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+  size = len(dataloader.dataset)
+  for batch, (X, y) in enumerate(dataloader):
+    X, y = X.to(device), y.to(device)
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+    # Compute prediction error
+    pred = model(X)
+    loss = loss_fn(pred, y)
 
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # Backpropagation
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    if batch % 100 == 0:
+      loss, current = loss.item(), batch * len(X)
+      print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
 def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+  size = len(dataloader.dataset)
+  num_batches = len(dataloader)
+  model.eval()
+  test_loss, correct = 0, 0
+  with torch.no_grad():
+    for X, y in dataloader:
+      X, y = X.to(device), y.to(device)
+      pred = model(X)
+      test_loss += loss_fn(pred, y).item()
+      correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+  test_loss /= num_batches
+  correct /= size
+  print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 if __name__ == "__main__":
-    # Get cpu or gpu device for training.
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print("Using {} device".format(device))
+  # Get cpu or gpu device for training.
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  print("Using {} device".format(device))
 
-    train_dataloader, test_dataloader = load_data()
-    model, loss_fn, optimizer = create_model()
-    for epoch in range(25):
-        train(train_dataloader, model, loss_fn, optimizer)
-        test(test_dataloader, model, loss_fn)
+  train_dataloader, test_dataloader = load_data()
+  model, loss_fn, optimizer = create_model()
+  for epoch in range(25):
+    train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
+  test(test_dataloader, model, loss_fn)
--- pytorch/tensor_experiments/main.py
+++ pytorch/tensor_experiments/main.py
@@ -9,53 +9,53 @@ import matplotlib.pyplot as plt
 def print_model(model,
                 named_parameters=False,
                 short=False):
-    if short:
-        print('\n'.join(model.__str__().split('\n')[:30]))
-    else:
-        print(model)
+  if short:
+    print('\n'.join(model.__str__().split('\n')[:30]))
+  else:
+    print(model)
 
-    if named_parameters:
-        if short:
-            for idx, (name, param) in enumerate(model.named_parameters()):
-                print("%30s | %s" % (name, param.size()))
-                if idx>=20:
-                    break
-        else:
-            for name, param in model.named_paramters():
-                print("%30s | %s" % (name, param.size()))
+  if named_parameters:
+    if short:
+      for idx, (name, param) in enumerate(model.named_parameters()):
+        print("%30s | %s" % (name, param.size()))
+        if idx>=20:
+          break
+    else:
+      for name, param in model.named_paramters():
+        print("%30s | %s" % (name, param.size()))
 
 
 def exp1():
-    a = torch.tensor([2., 3.], requires_grad=True)
-    b = torch.tensor([6., 4.], requires_grad=True)
-    Q = 3*a**3 - b**2
-    print(Q)
+  a = torch.tensor([2., 3.], requires_grad=True)
+  b = torch.tensor([6., 4.], requires_grad=True)
+  Q = 3*a**3 - b**2
+  print(Q)
 
-    # external_grad = torch.tensor([2., 2.])
-    # Q.backward(gradient=external_grad)
-    Q.sum().backward()
+  # external_grad = torch.tensor([2., 2.])
+  # Q.backward(gradient=external_grad)
+  Q.sum().backward()
 
-    print(9*a**2)
-    print(a.grad)
+  print(9*a**2)
+  print(a.grad)
 
-    print(-2*b)
-    print(b.grad)
+  print(-2*b)
+  print(b.grad)
 
-    print(9*a**2 == a.grad)
-    print(-2*b == b.grad)
+  print(9*a**2 == a.grad)
+  print(-2*b == b.grad)
 
 
 def exp2():
-    model = torchvision.models.resnet18(pretrained=True)
-    data = torch.rand(1, 3, 64, 64)
-    label = torch.rand(1, 1000)
-    print_model(model, named_parameters=True, short=True)
+  model = torchvision.models.resnet18(pretrained=True)
+  data = torch.rand(1, 3, 64, 64)
+  label = torch.rand(1, 1000)
+  print_model(model, named_parameters=True, short=True)
 
 
 if __name__ == "__main__":
-    # Get cpu or gpu device for training.
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print("Using {} device".format(device))
+  # Get cpu or gpu device for training.
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  print("Using {} device".format(device))
 
-    # exp1()
-    exp2()
+  # exp1()
+  exp2()
