2e2a29dc58b190c95ee24642f83e13d962f9b9f7
root <osmanaka038@gmail.com>
2021-07-28 04:26:59 +0000

    Learning rate experiment added.

--- pytorch/tensor_experiments/main.py
+++ pytorch/tensor_experiments/main.py
@@ -27,7 +27,7 @@ def print_model(model,
         print("%30s | %s" % (name, param.size()))
 
 
-def exp1():
+def exp_derivative():
   a = torch.tensor([2., 3.], requires_grad=True)
   b = torch.tensor([6., 4.], requires_grad=True)
   Q = 3*a**3 - b**2
@@ -47,11 +47,35 @@ def exp1():
   print(-2*b == b.grad)
 
 
-def exp2():
+def exp_learning_rate_check():
   model = torchvision.models.resnet18(pretrained=True)
   data = torch.rand(1, 3, 64, 64)
   label = torch.rand(1, 1000)
-  print_model(model, named_parameters=True, short=True)
+  print_model(model, model_desc=False,
+              named_parameters=True, short=False)
+
+  prediction = model(data)
+  loss = (prediction - label).sum()
+  loss.backward()
+  grad_values = model.fc.weight.grad.clone().detach()
+  print(loss)
+  print("Gradients of last layer:")
+  print(grad_values)
+
+  pre_values = model.fc.weight.clone().detach()
+  optim = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9)
+  optim.step()
+  cur_values = model.fc.weight.clone().detach()
+
+  diff_values = (cur_values - pre_values)
+  print("Last layer updates via SGD")
+  print(diff_values)
+
+  print("Update ratio:")
+  print(diff_values/grad_values)
+
+  expected_values = torch.ones_like(diff_values).fill_(-0.01)
+  print(torch.isclose((diff_values/grad_values), expected_values))
 
 
 if __name__ == "__main__":
@@ -59,5 +83,5 @@ if __name__ == "__main__":
   device = "cuda" if torch.cuda.is_available() else "cpu"
   print("Using {} device".format(device))
 
-  # exp1()
-  exp2()
+  # exp_derivative()
+  exp_learning_rate_check()
